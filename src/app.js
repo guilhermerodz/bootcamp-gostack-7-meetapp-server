18b88296edbe341ed880746fb54243f37eb35a15
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-26 01:34:04 -0300

    feat: added cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 
 import routes from './routes';
@@ -16,6 +17,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
