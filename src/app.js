73d40f0f2b63531b6d45a2252183c4be57c6d1ab
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-26 23:42:22 -0300

    feat: added exception handler

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import 'dotenv/config';
 
 import express from 'express';
+import 'express-async-errors';
+
 import cors from 'cors';
 import path from 'path';
+
+import Youch from 'youch';
+
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
 
 import routes from './routes';
 
@@ -12,8 +19,13 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
+    this.server.use(Sentry.Handlers.requestHandler());
     this.middlewares();
     this.routes();
+    this.server.use(Sentry.Handlers.errorHandler());
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -27,6 +39,18 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error ' });
+    });
   }
 }
 
